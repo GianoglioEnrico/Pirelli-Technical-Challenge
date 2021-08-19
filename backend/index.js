7cb83466fac66d228dd93a6c3eeeb3f7db1b0420
Enrico Gianoglio <enry8giano@gmail.com>
2021-08-19 16:06:53 +0200

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static("build"));
 const port = process.env.PORT || 4001;
 let measurement = [];
 let i = 0;
