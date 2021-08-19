2f9cbc789c42c3d72b566236f9b41b6a01eeb245
Enrico Gianoglio <enry8giano@gmail.com>
2021-08-19 15:57:26 +0200

    Added Filtering Feature and Header refactorization

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,64 @@
 import pirelliImg from "../assets/images/pirelli-logo.png";
-const Header = () => {
-  return (
-    <header className="App-header">
-      <img src={pirelliImg} alt="logo_pirelli" className="logo" />
-      <p>Pirelli Technical Challenge</p>
-    </header>
-  );
-};
+// const Header = () => {
+//   return (
+//     <header className="App-header">
+//       <img src={pirelliImg} alt="logo_pirelli" className="logo" />
+//       <p>Pirelli Technical Challenge</p>
+//     </header>
+//   );
+// };
 
-export default Header;
+// export default Header;
+
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+import IconButton from "@material-ui/core/IconButton";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  container: {
+    backgroundColor: "#000",
+
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+
+    color: "white",
+  },
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar className={classes.container}>
+          <IconButton
+            edge="start"
+            // className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <img src={pirelliImg} alt="logo_pirelli" className="logo" />
+          </IconButton>
+
+          <Typography color="inherit">Pirelli Technical Challenge</Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -7,6 +7,11 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 import socketIOClient from "socket.io-client";
 
@@ -23,6 +28,18 @@ const useStyles = makeStyles({
   table: {
     minWidth: 650,
   },
+  formControl: {
+    minWidth: 120,
+  },
+  circularLoaderContainer: {
+    marginTop: "20px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  circularLoader: {
+    color: "#000",
+  },
 });
 
 export default function DataTable() {
@@ -31,42 +48,122 @@ export default function DataTable() {
   const [filtered, setFiltered] = useState([]);
   const [filtering, setFiltering] = useState(false);
   const [car, setCar] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleFiltering = (e) => {
+    setCar(e.target.value);
+    if (e.target.value === "showAllCars") setFiltering(false);
+    else {
+      fetch(`http://localhost:4001/filter?car=${car}`)
+        .then((res) => res.json())
+        .then((response) => {
+          console.log(response);
+          setFiltered(response.data);
+          setFiltering(true);
+        });
+    }
+  };
+  console.log(filtered);
+  let fi = measurements.filter((f) => f.Car_id === car);
+  console.log(car);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   useEffect(() => {
     socket.on("FromAPI", (data) => {
       setmeasurements((prev) => [data, ...prev]);
+      setErrorMessage("");
     });
+    socket.on("connect_error", () => setErrorMessage("Connection Error"));
+    socket.on("connect_failed", () => setErrorMessage("Connection Failed"));
+
     return () => socket.disconnect();
   }, []);
 
   return (
-    <TableContainer className={classes.container} component={Paper}>
-      <Table stickyHeader className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Car</TableCell>
-            <TableCell align="right">Position</TableCell>
-            <TableCell align="right">Temperature</TableCell>
-            <TableCell align="right">Omega</TableCell>
-            <TableCell align="right">Speed</TableCell>
-            <TableCell align="right">Pressure</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody className={classes.tbody}>
-          {measurements.length > 0 &&
-            measurements.map((measure, index) => (
-              <TableRow key={index}>
-                <TableCell component="th" scope="row">
-                  {measure.Car_id}
-                </TableCell>
-                <TableCell align="right">{measure.Position}</TableCell>
-                <TableCell align="right">{measure.Temp}</TableCell>
-                <TableCell align="right">{measure.Omega}</TableCell>
-                <TableCell align="right">{measure.Speed}</TableCell>
-                <TableCell align="right">{measure.Press}</TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-controlled-open-select-label">Car</InputLabel>
+        <Select
+          labelId="demo-controlled-open-select-label"
+          id="demo-controlled-open-select"
+          open={open}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          value={car}
+          onChange={(e) => {
+            handleFiltering(e);
+            fi = [];
+          }}
+        >
+          <MenuItem value={"showAllCars"}>
+            <em>Show All Cars</em>
+          </MenuItem>
+          <MenuItem value={"Volvo V40"}>Volvo V40</MenuItem>
+          <MenuItem value={"Audi A6"}>Audi A6</MenuItem>
+          <MenuItem value={"Porsche 911"}>Porsche 911</MenuItem>
+        </Select>
+      </FormControl>
+
+      <TableContainer className={classes.container} component={Paper}>
+        <Table stickyHeader className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Car</TableCell>
+              <TableCell align="right">Position</TableCell>
+              <TableCell align="right">Temperature</TableCell>
+              <TableCell align="right">Omega</TableCell>
+              <TableCell align="right">Speed</TableCell>
+              <TableCell align="right">Pressure</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody className={classes.tbody}>
+            {!filtering &&
+              measurements.length > 0 &&
+              measurements.map((measure, index) => (
+                <TableRow key={index}>
+                  <TableCell component="th" scope="row">
+                    {measure.Car_id}
+                  </TableCell>
+                  <TableCell align="right">{measure.Position}</TableCell>
+                  <TableCell align="right">{measure.Temp}</TableCell>
+                  <TableCell align="right">{measure.Omega}</TableCell>
+                  <TableCell align="right">{measure.Speed}</TableCell>
+                  <TableCell align="right">{measure.Press}</TableCell>
+                </TableRow>
+              ))}
+
+            {filtering &&
+              fi.length > 0 &&
+              fi.map((fil, index) => (
+                <TableRow key={index}>
+                  <TableCell component="th" scope="row">
+                    {fil.Car_id}
+                  </TableCell>
+                  <TableCell align="right">{fil.Position}</TableCell>
+                  <TableCell align="right">{fil.Temp}</TableCell>
+                  <TableCell align="right">{fil.Omega}</TableCell>
+                  <TableCell align="right">{fil.Speed}</TableCell>
+                  <TableCell align="right">{fil.Press}</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {!errorMessage && measurements.length < 1 && (
+        <div className={classes.circularLoaderContainer}>
+          <CircularProgress className={classes.circularLoader} />
+        </div>
+      )}
+      {errorMessage && <h1 style={{ color: "red" }}>{errorMessage}</h1>}
+    </div>
   );
 }
